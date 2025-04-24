7646d0465cc543ecc9e89f3ab60589a1d832cfa3
VishalSuvarnakanti <vishalsuvarnakanti@gmail.com>
2025-04-24 12:04:11 -0500

    Update3
    Split the numbers in the lists using split method.

--- PR1.py
+++ PR1.py
@@ -2,11 +2,18 @@ def arithmetic_arranger(problems, show_answers=False):
 
     if len(problems)>5:
         return "Error. Too many problems"
-
-    num3 = []
-
-    return num3
     
+    line1 = []
+    
+    for part in problems:
+        if '+' in part:
+            word = part.split('+')
+        else:
+            word = part.split('-')
+        
+        line1.append(word)
+    
+    return line1
     
 
 sum = arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"])
