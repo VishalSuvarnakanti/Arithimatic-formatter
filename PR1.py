5317b0671c802fca5ab18509a2bb4fddf2f1e940
VishalSuvarnakanti <vishalsuvarnakanti@gmail.com>
2025-04-24 13:52:04 -0500

    Update4
    Attempted to return a list of just the answers. Issue with the subtraction.

--- PR1.py
+++ PR1.py
@@ -1,3 +1,13 @@
+def addition(word):
+    sum = int(word[0]) + int(word[1])
+    return sum
+
+def subtraction(word):
+    num1 = int(word[0])
+    num2 = int(word[1])
+    difference = num1-num2
+    return difference
+
 def arithmetic_arranger(problems, show_answers=False):
 
     if len(problems)>5:
@@ -8,14 +18,15 @@ def arithmetic_arranger(problems, show_answers=False):
     for part in problems:
         if '+' in part:
             word = part.split('+')
+            sum = addition(word)
+            line1.append(sum)
         else:
             word = part.split('-')
-        
-        line1.append(word)
-    
+            difference = subtraction(word)
+            line1.append(word)
+            
     return line1
     
-
 sum = arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"])
 print(sum)
 
