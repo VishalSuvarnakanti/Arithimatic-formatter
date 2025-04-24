9d9d3d1af34a45b4b8db00cb8195d6b3f0a51bd8
VishalSuvarnakanti <vishalsuvarnakanti@gmail.com>
2025-04-24 10:58:46 -0500

    PR2.py
    Removed the failed grouping attempt.

--- PR1.py
+++ PR1.py
@@ -5,17 +5,6 @@ def arithmetic_arranger(problems, show_answers=False):
 
     num3 = []
 
-    for i in problems:
-        num2 = []
-        for n in i:
-            num = []
-            if n.isdigit():
-                num.append(int(n))
-            else:
-                continue
-            num2.append(num)
-        num3.append(num2)
-
     return num3
     
     
