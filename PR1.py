e3520caf0b242b60e88b78b517e7097323c7d3b5
VishalSuvarnakanti <vishalsuvarnakanti@gmail.com>
2025-05-05 14:14:28 -0500

    Update19
    Redid the string formatting in the main function instead.

--- PR1.py
+++ PR1.py
@@ -57,7 +57,8 @@ def arithmetic_arranger(problems, show_answers=False):
     first = ''
     second = ''
     lines = ''
-
+    sumx = ''
+    answer = ''
     
     for part in problems:
         problem = part.split()
@@ -95,11 +96,30 @@ def arithmetic_arranger(problems, show_answers=False):
         
         length = max(len(operand1), len(operand2))
         top = str(operand1).rjust(length)
-        string = operator + str(operand2).rjust(length-1)
+        bottom = operator + str(operand2).rjust(length-1)
         line = ""
         result = str(sum).rjust(length)
 
-    return print_lines(line1, line2, line3, show_answers)
+        for i in range(length):
+            line+='-'
+
+        if part != problems[-1]:
+            first += top+'    '
+            second += bottom+'    '
+            lines += line+'    '
+            sumx += result+'    '
+        else:
+            first+=top
+            second+=bottom
+            lines+=line
+            sumx+=result
+
+        if show_answers:
+            answer = first + '\n' + bottom + '\n' + lines + '\n' + sumx 
+        else:
+            answer = first + '\n' + bottom + '\n' + lines
+
+    return answer
 
             
 print(arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"],True))
