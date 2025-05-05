7a9d4fa67610365fc75c5bb9a0a6801c47af85ba
VishalSuvarnakanti <vishalsuvarnakanti@gmail.com>
2025-05-04 23:16:01 -0500

    Update17
    Commented all the custom functions to attempt to implement all of them as well in the main function instead.

--- PR1.py
+++ PR1.py
@@ -1,28 +1,28 @@
 import re
 
-def print_lines(line1, line2, line3, show_answers):
-    newline1 = [str(i) for i in line1]
-    newline2 = [str(i) for i in line2]
-    newline3 = [str(i) for i in line3]
-    final_line = [newline1, newline2, newline3]
-
-    output_lines = []
-
-    if show_answers==False:
-        for i in range(len(final_line)-1):
-            line = ''
-            for n in final_line[i]:
-               line += n.rjust(8)
-            output_lines.append(line)
-    else:
-        for i in range(len(final_line)+1):
-            line = ''
-            for n in final_line[i]:
-                if i==2:
-                    line+='------'.rjust(8)
-                else:
-                    line += n.rjust(8)
-            output_lines.append(line)
+#def print_lines(line1, line2, line3, show_answers):
+#    newline1 = [str(i) for i in line1]
+#    newline2 = [str(i) for i in line2]
+#    newline3 = [str(i) for i in line3]
+#    final_line = [newline1, newline2, newline3]
+#
+#    output_lines = []
+#
+#    if show_answers==False:
+#        for i in range(len(final_line)-1):
+#            line = ''
+#            for n in final_line[i]:
+#               line += n.rjust(8)
+#            output_lines.append(line)
+#    else:
+#        for i in range(len(final_line)+1):
+#            line = ''
+#            for n in final_line[i]:
+#                if i==2:
+#                    line+='------'.rjust(8)
+#                else:
+#                    line += n.rjust(8)
+#            output_lines.append(line)
 
         
 
@@ -33,17 +33,17 @@ def print_lines(line1, line2, line3, show_answers):
 #        output_lines.append(line)
               
 
-    return '\n'.join(output_lines)
+#    return '\n'.join(output_lines)
 
-def addition(word):
-    sum = int(word[0]) + int(word[1])
-    return sum
+#def addition(word):
+#    sum = int(word[0]) + int(word[1])
+#    return sum
 
-def subtraction(word2):
-    num1 = int(word2[0])
-    num2 = int(word2[1])
-    difference = num1 - num2
-    return difference
+#def subtraction(word2):
+#    num1 = int(word2[0])
+#    num2 = int(word2[1])
+#    difference = num1 - num2
+#    return difference
 
 def arithmetic_arranger(problems, show_answers=False):
 
@@ -87,7 +87,7 @@ def arithmetic_arranger(problems, show_answers=False):
         else:
             return "Error"
         
-        
+
     return print_lines(line1, line2, line3, show_answers)
 
             
