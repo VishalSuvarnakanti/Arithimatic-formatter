3170a9384786cebeb11154c8b402ccf9029eb76d
VishalSuvarnakanti <vishalsuvarnakanti@gmail.com>
2025-05-05 14:27:56 -0500

    SmallUpdate7
    Fixed some logical errors

--- PR1.py
+++ PR1.py
@@ -5,10 +5,6 @@ def arithmetic_arranger(problems, show_answers=False):
     if len(problems)>5:
         return "Error. Too many problems"
 
-    line1 = []
-    line2 = []
-    line3 = []
-
     first = ''
     second = ''
     lines = ''
@@ -19,7 +15,7 @@ def arithmetic_arranger(problems, show_answers=False):
         problem = part.split()
         operand1, operator, operand2 = problem
         sum = ""
-        if (re.search("[^\s0-9.+-", part)):
+        if (re.search("[^\s0-9.+-]", part)):
             return "Error: Numbers must only contain digits"
         elif (re.search("[/]", part) or re.search("[*]", part)):
             return "Error: Operator must be '+' or '-'"
@@ -33,30 +29,30 @@ def arithmetic_arranger(problems, show_answers=False):
         else:
             return "Error"
         
-        length = max(len(operand1), len(operand2))
+        length = max(len(operand1), len(operand2)) + 2
         top = str(operand1).rjust(length)
         bottom = operator + str(operand2).rjust(length-1)
         line = ""
         result = str(sum).rjust(length)
 
         for i in range(length):
-            line+='-'
+            line += "-"
 
         if part != problems[-1]:
-            first += top+'    '
-            second += bottom+'    '
-            lines += line+'    '
-            sumx += result+'    '
+            first += top + '    '
+            second += bottom + '    '
+            lines += line + '    '
+            sumx += result + '    '
         else:
-            first+=top
-            second+=bottom
-            lines+=line
-            sumx+=result
+            first += top
+            second += bottom
+            lines += line
+            sumx += result
 
-        if show_answers:
-            answer = first + '\n' + bottom + '\n' + lines + '\n' + sumx 
-        else:
-            answer = first + '\n' + bottom + '\n' + lines
+    if show_answers:
+        answer = first + '\n' + second + '\n' + lines + '\n' + sumx 
+    else:
+        answer = first + '\n' + second + '\n' + lines
 
     return answer
 
